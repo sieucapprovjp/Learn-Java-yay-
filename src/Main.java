c9b0762cfee3459ce7b068675eb6a32f166564fe
htran <htranquang6@gmail.com>
2026-03-19 09:39:04 +0700

    yay

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        double width = 0;
-        double height = 0;
-        double area = 0;
-
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter your width :");
-        width = scanner.nextDouble();
+        String adj1;
+        String noun1;
+        String adj2;
+        String Verb1;
+        String adj3;
 
-        System.out.println("Enter yout height :");
-        height = scanner.nextDouble();
-
-        area = width * height;
-
-        System.out.println("The area is: "+ area + "cm^2");
 
         scanner.close();
     }
